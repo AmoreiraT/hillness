3d1cd8519551162245c38f05c44c56f374c4519c
Amoreirat <thiago.m.hq@gmail.com>
2023-07-29 05:04:56 -0300

    api update

--- src/features/CanvasContainer/repository/index.ts
+++ src/features/CanvasContainer/repository/index.ts
@@ -20,31 +20,13 @@ export const useFetchCovidDataRepository = (): FetchCovidDataRepository => {
     );
   }
   const fetchCovidData = async (): Promise<CovidDataEntity[]> => {
-    const startDate = new Date('2020-02-25');
-    const endDate = new Date();
-    const oneDay = 24 * 60 * 60 * 1000;
-    const covidDataArray = [];
+   const response = await axios.get(`http://localhost:3002/covid/db`);
 
-    for (
-      let currentDate = startDate;
-      currentDate <= endDate;
-      currentDate = new Date(currentDate.getTime() + oneDay)
-    ) {
-      const formattedDate = currentDate
-        .toISOString()
-        .split('T')[0]
-        .replace(/-/g, '');
-      const response = await axios.get(
-        `https://covid19-brazil-api.now.sh/api/report/v1/brazil/${formattedDate}`
-      );
-
-      if (response.data && response.data.data) {
-        const { cases, deaths } = sumCasesAndDeaths(response.data.data);
-        covidDataArray.push({ date: formattedDate, cases, deaths });
-      }
-    }
-
-    return covidDataArray.map(mapCovidDataDtoToEntity);
+   if (response.data) {
+     return response.data.map(mapCovidDataDtoToEntity);
+   } else {
+     throw new Error('API response is undefined');
+   }
   };
 
   return {
--- src/features/Index/presentation/inicial.tsx
+++ src/features/Index/presentation/inicial.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import CanvasContainer from '../../CanvasContainer/presentation';
 import { usePageTitle } from '../../../shared/components/PageTitle/state';
+import React from 'react';
 
 export function InitialPage() {
   const { setPageTitleElement } = usePageTitle();
