ef7d121d92b854cbbb389fa57a0cb7261a215c74
Amoreirat <thiago.m.hq@gmail.com>
2023-10-03 04:19:12 -0300

    ok

--- src/features/CanvasContainer/presentation/state/useCovid.ts
+++ src/features/CanvasContainer/presentation/state/useCovid.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { CovidDataEntity } from '../../entities/entity';
 import { ErrorType } from '../../../../core/error';
 
-export const initialCovidDataState: [CovidDataEntity] = [
+export const initialCovidDataState: CovidDataEntity[] = [
   {
     date: 0,
     cases: 0,
@@ -11,15 +11,15 @@ export const initialCovidDataState: [CovidDataEntity] = [
 ];
 
 export interface CovidDataState {
-  covidData: [CovidDataEntity];
+  covidData: CovidDataEntity[];
   error: ErrorType | undefined;
-  setCovidData: (value: [CovidDataEntity]) => void;
+  setCovidData: (value: CovidDataEntity[]) => void;
   setError: (error: ErrorType | undefined) => void;
 }
 
 export const useCovidDataStore = create<CovidDataState>((set) => ({
   covidData: initialCovidDataState,
   error: undefined,
-  setCovidData: (value: [CovidDataEntity]) => set({ covidData: value }),
+  setCovidData: (value: CovidDataEntity[]) => set({ covidData: value }),
   setError: (error: ErrorType | undefined) => set({ error }),
 }));
